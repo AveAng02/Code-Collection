fc23d25d0a2d2f1ebdb04aeeb4167d8aeb75099c
ayan-acharya <ayan.acharya02@gmail.com>
2022-12-09 14:27:23 +0530

    First Triangle

--- C and C++/OpenGL/OpenGL_playground/Main.cpp
+++ C and C++/OpenGL/OpenGL_playground/Main.cpp
@@ -40,7 +40,7 @@ int main()
 	GLfloat vertices[] =
 	{
 		-0.5f, -0.5f, 0.0f, // left corner
-		0.5f, 0.5f, 0.0f, // right corner
+		0.5f, -0.5f, 0.0f, // right corner
 		0.0f, 0.5f, 0.0f // top corner
 	};
 
@@ -62,6 +62,54 @@ int main()
 	glViewport(0, 0, 720, 480);
 
 
+	////////////////////////////////////// Shader Work /////////////////////////////
+	// GLuint is a custom type defined in OpenGL library
+	// this line defines a new shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+
+	glCompileShader(vertexShader);
+
+
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+	glCompileShader(fragmentShader);
+
+
+
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	///////////////////////////////////////////////////////////////////////////////
+
+
+	GLuint VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+
 	glClearColor(0.225f, 0.0f, 0.0f, 1.0f); // specify the color the background
 	glClear(GL_COLOR_BUFFER_BIT); // clean the back buffer and assign new color to it
 	glfwSwapBuffers(window1); // swap the back and front buffers
@@ -69,9 +117,23 @@ int main()
 
 	while (!glfwWindowShouldClose(window1)) // infinite loop to keep the window open until close button is pressed
 	{
+		glClearColor(0.225f, 0.0f, 0.0f, 1.0f); // specify the color the background
+		glClear(GL_COLOR_BUFFER_BIT); // clean the back buffer and assign new color to it
+
+		glUseProgram(shaderProgram);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window1); // swap the back and front buffers
+
+
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwDestroyWindow(window1); // marks the end of window creation
 	glfwTerminate(); // terminating glfw
