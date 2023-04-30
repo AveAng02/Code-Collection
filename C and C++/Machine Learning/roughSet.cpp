399b55583f5d8ad93299dd40f43e7bb13e90f28a
Ayan Acharya <ayan.acharya02@gmail.com>
2023-04-30 12:31:32 +0530

    function fower approximation of rough set added

--- C and C++/Machine Learning/roughSet.cpp
+++ C and C++/Machine Learning/roughSet.cpp
@@ -9,6 +9,31 @@
 
 
 
+void print_1d_string_vec(std::vector<std::string> data)
+{
+    std::cout << " { ";
+
+    for(int i = 0, j = 0; i < data.size() - 1; i++)
+    {
+        std::cout << data[i] << ", ";
+    }
+
+    std::cout << data[data.size() - 1] << " } " << std::endl;
+}
+
+
+void print_2d_string_vec(std::vector<std::vector<std::string>> data)
+{
+    for(int i = 0, j = 0; i < data.size(); i++)
+    {
+        print_1d_string_vec(data[i]);
+
+        std::cout << "\n";
+    }
+}
+
+
+
 // Importing required libraries
 // Load the dataset
 // Convert the dataset to a array
@@ -65,6 +90,35 @@ std::vector<std::string> unique(std::vector<std::string> field)
 
 
 // Define the lower and upper approximation functions
+std::vector<std::string> lower_approx
+(std::vector<std::vector<std::string>> data, int universe_line,
+std::string decision, int decision_line)
+{
+    std::vector<std::string> lower_set;
+
+    for(int i = 0; i < data.size(); i++)
+    {
+        if(data[i][decision_line] == decision)
+        {
+            lower_set.push_back(data[i][universe_line]);
+        }
+    }
+
+    // std::cout << "Lower Set for " << decision << std::endl;
+    // print_1d_string_vec(lower_set);
+
+    return lower_set;
+}
+
+
+std::vector<std::string> upper_approx
+(std::vector<std::vector<std::string>> data, 
+int universe_line, std::string decision, int decision_line)
+{
+    std::vector<std::string> upper_set;
+
+    return upper_set;
+}
 
 
 // Define the universe of discourse
@@ -82,26 +136,45 @@ private:
 };
 
 // convert this function to a class
+// this function creates a roughset
 void create_rough_set(std::vector<std::vector<std::string>> data)
 {
     int attributes = data[0].size(); // stores the number of attributes
 
     std::vector<std::string> universe;
+    std::vector<std::string> decision;
 
     for(int i = 1; i < data.size(); i++)
     {
-        // this loop defines the universe 
-        std::string val = data[i][data[0].size() - 1];
+        std::string val;
+
+        // this block defines the decision 
+        val = data[i][data[0].size() - 1];
+        decision.push_back(val);
+
+        // this block defines the universe
+        val = data[i][0];
         universe.push_back(val);
     }
 
     universe = unique(universe); // vector only retains the unique elements
+    decision = unique(decision);
+
+    std::vector<std::vector<std::string>> lower_set;
+    std::vector<std::vector<std::string>> upper_set;
+
+    for(int i = 0; i < decision.size(); i++)
+    {
+        lower_set.push_back(lower_approx(data, 1, decision[i], data[0].size() - 1));
+        upper_set.push_back(lower_approx(data, 1, decision[i], data[0].size() - 1));
+    }
 
     /*
-    for(int i = 0; i < universe.size(); i++)
-    {
-        std::cout << universe[i] << std::endl;
-    }
+    std::cout << "Universe:\n";
+    print_1d_string_vec(universe);
+
+    std::cout << "Decision:\n";
+    print_1d_string_vec(decision);
     */
 
 
@@ -121,17 +194,7 @@ int main()
 
     create_rough_set(data);
 
-    /*
-    for(int j = 0, i = 0; i < data.size(); i++)
-    {
-        for(j = 0; j < data[i].size(); j++)
-        {
-            std::cout << data[i][j] << "  ";
-        }
-
-        std::cout << "\n";
-    }
-    */
+    // print_2d_string_vec(data);
 
     // std::cout << data.size() << "   " << data[0].size() << std::endl;
 }
