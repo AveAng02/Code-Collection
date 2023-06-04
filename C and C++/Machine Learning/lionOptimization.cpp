17e19b1d9bf83f613b0f335142b66c70a3ae9b8e
Ayan Acharya <ayan.acharya02@gmail.com>
2023-06-04 16:41:11 +0530

    Lion Optimization Pilot

---
+++ C and C++/Machine Learning/lionOptimization.cpp
@@ -0,0 +1,75 @@
+
+#include <vector>
+#include <unordered_map>
+
+
+
+enum lionType
+{
+    RESIDENT,
+    NOMAD
+};
+
+
+class Prey
+{
+public:
+    Prey()
+    {
+
+    }
+
+
+private:
+    std::unordered_map<double, double> currentPos;
+    std::vector<std::unordered_map<double, double>> positionList;
+};
+
+
+class Lion
+{
+public:
+    Lion()
+    {
+
+    }
+
+private:
+    std::unordered_map<double, double> currentPos;
+    std::vector<std::unordered_map<double, double>> positionList;
+    bool gender;
+};
+
+
+class Pride
+{
+public:
+    Pride()
+    {
+        // Initialization
+    }
+
+    void convergence();
+    void populationEqui();
+
+private:
+    std::vector<Lion> groups;
+    int prideSz;
+};
+
+
+void Hunting(Lion, Prey);
+void Roaming(Lion);
+void Mating(Lion, Lion);
+
+
+
+int main()
+{
+
+
+    return 0;
+}
+
+
+
