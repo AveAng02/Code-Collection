5953426a2b74b531de99caca17432b5cfe942974
ayan-acharya <ayan.acharya02@gmail.com>
2024-01-05 22:23:56 +0530

    Updating rangedPrime code

--- C and C++/rangedPrime.cpp
+++ C and C++/rangedPrime.cpp
@@ -1,15 +1,8 @@
 
 #include <iostream>
-#include <pthread.h>
+#include <thread>
 #include <stdexcept>
 #include <chrono>
-
-
-struct limitDivision
-{
-    uint32_t lower;
-    uint32_t upper;
-};
 
 
 bool isPrime(uint32_t num)
@@ -26,27 +19,77 @@ bool isPrime(uint32_t num)
 }
 
 
-void *printPrimes(void *threadID)
+void printPrimes(uint32_t threadID, uint32_t lowerLimit, uint32_t upperLimit)
 {
-    struct limitDivision *data = (struct limitDivision *)threadID;
+    std::cout << "Result of thread " << threadID << std::endl;
 
-    for(uint32_t i = data->lower; i <= data->upper; i++)
+    for(uint32_t i = lowerLimit; i <= upperLimit; i++)
     {
         if(isPrime(i))
         {
             std::cout << "  " << i << "   " << std::endl;
         }
     }
+}
 
-    pthread_exit(NULL);
-    return NULL;
+
+void createThread(uint32_t lwrLmt, uint32_t uprLmt, uint32_t id)
+{
+    std::thread newThread(printPrimes, id, lwrLmt, uprLmt);
+    std::cout << "New Thread created with ID : " << id << std::endl;
+    newThread.join();
+}
+
+
+std::chrono::duration<double> rangedPrimes(uint32_t lwrLmt, uint32_t uprLmt, uint32_t numOfThrds)
+{
+    uint32_t *upprBoundList = new uint32_t[numOfThrds];
+    uint32_t *lwrBoundList = new uint32_t[numOfThrds];
+    uint32_t division = ((uprLmt - lwrLmt) / numOfThrds);
+
+    // Starting clock
+    auto start = std::chrono::steady_clock::now();
+
+    // Dividing the range among all the threads
+    // Loop to set the lower bounds for each thread
+    for(uint32_t i = 0; i < numOfThrds; i++)
+    {
+        lwrBoundList[i] = lwrLmt + (i * division);
+    }
+
+    // Loop to set the upper bounds for each thread
+    for(uint32_t i = 1; i < numOfThrds; i++)
+    {
+        if((lwrBoundList[i] - 1) > uprLmt)
+        {
+            upprBoundList[i - 1] = lwrBoundList[i] - 1;
+        }
+        else
+        {
+            upprBoundList[i - 1] = uprLmt;
+        }
+    }
+
+    upprBoundList[numOfThrds - 1] = uprLmt;
+
+    // creating threads
+    for(uint32_t i = 0; i < numOfThrds; i++)
+    {
+        createThread(lwrBoundList[i], upprBoundList[i], i);
+    }
+
+    // Stopping clock
+    auto stop = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> diff = stop - start;
+
+    return diff;
 }
 
 
 int main()
 {
     uint32_t rngUpper = 0, rngLower = 0, threadCount = 0;
-    int rc;
 
     std::cout << "Input lower limit of range : ";
     std::cin >> rngLower;
@@ -62,45 +105,7 @@ int main()
         throw std::logic_error("Invalid range");
     }
 
-    // Defining list of threads and limits
-    pthread_t threadList[threadCount];
-    limitDivision limitList[threadCount];
-
-    // Starting clock
-    auto start = std::chrono::steady_clock::now();
-
-    uint32_t beginning = rngLower, division = (((rngUpper + threadCount - 1) - rngLower) / threadCount);
-
-    // Dividing the range among all the threads
-    // TODO : the division of range algorithm is wrong. Fix it.
-    for(uint32_t i = 1; i <= threadCount; i++)
-    {
-        limitList[i - 1].lower = beginning;
-        limitList[i - 1].upper = ((beginning + division) <= rngUpper)? (beginning + division): rngUpper;
-
-        beginning = limitList[i - 1].upper + 1;
-    }
-
-    /*
-    for(uint32_t i = 0; i < threadCount; i++)
-    {
-        std::cout << "ID = " << i << "\tLower : " << limitList[i].lower << "\tUpper : " << limitList[i].upper << std::endl;
-    }
-    */
-
-    // creating threads
-    for(uint32_t i = 0; i < threadCount; i++)
-    {
-        if(pthread_create(&threadList[i], NULL, printPrimes, (void *)&limitList[i]) != 0)
-        {
-            std::cout << "failed to create threads" << std::endl;
-            return -1;
-        }
-    }
-
-    // Stopping clock
-    auto stop = std::chrono::steady_clock::now();
-    std::chrono::duration<double> diff = stop - start;
+    std::chrono::duration<double> diff = rangedPrimes(rngLower, rngUpper, threadCount);
 
     std::cout << "Time taken : " << std::chrono::duration<double, std::milli>(diff).count() << " ms" << std::endl;
 
