ad16151e5c743a61828b814828536c402dd98adc
ayan-acharya <ayan.acharya02@gmail.com>
2024-01-06 19:46:39 +0530

    replaced pthreads with std::thread

--- C and C++/rangedPrime.cpp
+++ C and C++/rangedPrime.cpp
@@ -4,6 +4,8 @@
 #include <stdexcept>
 #include <chrono>
 
+uint32_t counter = 0;
+
 bool isPrime(uint32_t num)
 {
     for(uint32_t i = 2; i < num; i++)
@@ -19,23 +21,24 @@ void printPrimes(uint32_t threadID, uint32_t lowerLimit, uint32_t upperLimit)
     for(uint32_t i = lowerLimit; i <= upperLimit; i++)
         if(isPrime(i))
             std::cout << "  " << i << "   " << std::endl;
+
+    counter++;
 }
 
 void createThread(uint32_t lwrLmt, uint32_t uprLmt, uint32_t id)
 {
     std::thread newThread(printPrimes, id, lwrLmt, uprLmt);
     std::cout << "New Thread created with ID : " << id << std::endl;
-    newThread.join();
+    newThread.detach();
 }
 
-std::chrono::duration<double> rangedPrimes(uint32_t lwrLmt, uint32_t uprLmt, uint32_t numOfThrds)
+void rangedPrimes(uint32_t lwrLmt, uint32_t uprLmt, uint32_t numOfThrds)
 {
+    std::cout << "Function Starts" << std::endl;
+
     uint32_t *upprBoundList = new uint32_t[numOfThrds];
     uint32_t *lwrBoundList = new uint32_t[numOfThrds];
     uint32_t division = ((uprLmt - lwrLmt) / numOfThrds);
-
-    // Starting clock
-    auto start = std::chrono::steady_clock::now();
 
     // Dividing the range among all the threads
     // Loop to set the lower bounds for each thread
@@ -48,29 +51,20 @@ std::chrono::duration<double> rangedPrimes(uint32_t lwrLmt, uint32_t uprLmt, uin
     for(uint32_t i = 1; i < numOfThrds; i++)
     {
         if((lwrBoundList[i] - 1) > uprLmt)
-        {
             upprBoundList[i - 1] = lwrBoundList[i] - 1;
-        }
         else
-        {
             upprBoundList[i - 1] = uprLmt;
-        }
     }
 
     upprBoundList[numOfThrds - 1] = uprLmt;
 
     // creating threads
     for(uint32_t i = 0; i < numOfThrds; i++)
-    {
         createThread(lwrBoundList[i], upprBoundList[i], i);
-    }
 
-    // Stopping clock
-    auto stop = std::chrono::steady_clock::now();
+    while(counter < numOfThrds);
 
-    std::chrono::duration<double> diff = stop - start;
-
-    return diff;
+    std::cout << "Function Ends" << std::endl;
 }
 
 int main()
@@ -87,15 +81,14 @@ int main()
 
     // Checking to make sure that the upperlimit is bigger than the lowerlimit
     if (rngUpper < rngLower)
-    {
         throw std::logic_error("Invalid range");
-    }
 
-    std::chrono::duration<double> diff = rangedPrimes(rngLower, rngUpper, threadCount);
+    auto start = std::chrono::steady_clock::now(); // Starting clock
+    rangedPrimes(rngLower, rngUpper, threadCount);
+    auto stop = std::chrono::steady_clock::now(); // Stopping clock
+    std::chrono::duration<double> diff = stop - start;
 
     std::cout << "Time taken : " << std::chrono::duration<double, std::milli>(diff).count() << " ms" << std::endl;
 
     return 0;
 }
-
-
