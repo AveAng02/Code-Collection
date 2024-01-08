7c23c1b63165b046ad85a068e245a1ac3e12aae5
ayan-acharya <ayan.acharya02@gmail.com>
2024-01-09 01:04:50 +0530

    Slight optimization

--- C and C++/rangedPrime.cpp
+++ C and C++/rangedPrime.cpp
@@ -7,22 +7,22 @@
 #include <memory>
 #include <cmath>
 
-bool isPrime(uint32_t num)
+bool isPrime(const uint32_t num)
 {
     if(num <= 1) return false;
     if(num <= 3) return true;
     if(num % 2 == 0 || num % 3 == 0) return false;
 
-    num = (uint32_t)::sqrt(num) + 1;
+    uint32_t num2 = (uint32_t)::sqrt(num) + 1;
 
-    for(uint32_t i = 5; i < num; i += 6)
+    for(uint32_t i = 5; i < num2; i += 6)
         if (num % i == 0)
             return false;
     
     return true;
 }
 
-void printPrimes(uint32_t threadID, std::vector<uint32_t>& list, uint32_t lowerLimit, uint32_t upperLimit)
+void printPrimes(const uint32_t threadID, std::vector<uint32_t>& list, const uint32_t lowerLimit, const uint32_t upperLimit)
 {
     for(uint32_t i = lowerLimit; i <= upperLimit; i++)
         if(isPrime(i))
@@ -32,6 +32,7 @@ void printPrimes(uint32_t threadID, std::vector<uint32_t>& list, uint32_t lowerL
 int main()
 {
     uint32_t rngUpper = 10000000, rngLower = 1, threadCount = 16;
+    std::vector<uint32_t> primeList;
 
     std::cout << "Upper : " << rngUpper << "\nLower : " << rngLower << std::endl;
 
@@ -41,21 +42,19 @@ int main()
     uint32_t *upprBoundList = new uint32_t[threadCount];
     uint32_t *lwrBoundList = new uint32_t[threadCount];
     uint32_t division = ((rngUpper - rngLower) / threadCount);
-    std::vector<uint32_t> primeList;
     std::vector<std::vector<uint32_t>> valueList (threadCount);
     std::vector<std::thread> threadList (threadCount);
 
     // Dividing the range among all the threads
-    for(uint32_t i = 0; i < threadCount; i++)
-        lwrBoundList[i] = rngLower + (i * division); // Loop to set the lower bounds for each thread
+    lwrBoundList[0] = rngLower; // Setting the lowest lower bound
+    upprBoundList[threadCount - 1] = rngUpper; // Setting the higgest upper bound
 
-    for(uint32_t i = 1; i < threadCount; i++) // Loop to set the upper bounds for each thread
-        if((lwrBoundList[i] - 1) < rngUpper)
-            upprBoundList[i - 1] = lwrBoundList[i] - 1;
-        else
-            upprBoundList[i - 1] = rngUpper;
-
-    upprBoundList[threadCount - 1] = rngUpper;
+    // Loop to define the division of ranges between upper and lower bound
+    for(uint32_t i = 1; i < threadCount; i++)
+    {
+        lwrBoundList[i] = rngLower + (i * division); 
+        upprBoundList[i - 1] = ((lwrBoundList[i] - 1) < rngUpper)? lwrBoundList[i] - 1 : rngUpper;
+    }
 
     auto start = std::chrono::steady_clock::now(); // Starting clock
 
@@ -63,6 +62,7 @@ int main()
     for(uint32_t i = 0; i < threadCount; i++)
         threadList[i] = std::thread(printPrimes, i, std::ref(valueList[i]), lwrBoundList[i], upprBoundList[i]);
 
+    // Joining the threads to create a thread fork
     for(uint32_t i = 0; i < threadList.size(); i++)
         threadList[i].join();
 
@@ -74,26 +74,15 @@ int main()
             primeList.push_back(valueList[i][j]);
 
     // Reference code
-
-    std::vector<uint32_t> correctnessList;
-
-    for(uint32_t i = rngLower; i < rngUpper; i++)
+    for(uint32_t c = 0, i = rngLower; i <= rngUpper; i++)
         if(isPrime(i))
-            correctnessList.push_back(i);
-
-    for(uint32_t j = 0; j < correctnessList.size(); j++)
-    {
-        if(correctnessList[j] != primeList[j])
         {
-            std::cout << "Discrepency found at " << j << " position; value of correctnessList : " << correctnessList[j] << " ; primeList : " << primeList[j] << std::endl;
+            if(i != primeList[c])
+                std::cout << "Discrepency found at " << c << " position; value of correctnessList : " << i << " ; primeList : " << primeList[c] << std::endl;
+            c++;
         }
-    }
 
-    /////////////////
-
-    std::chrono::duration<double> diff = stop - start;
-
-    std::cout << "Time : " << std::chrono::duration<double, std::milli>(diff).count() << std::endl;
+    std::cout << "Time : " << std::chrono::duration<double, std::milli>(stop - start).count() << std::endl;
 
     return 0;
 }
