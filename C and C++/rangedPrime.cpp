8e1d23c9f27441921b9b3d4c55cecff92b70bade
ayan-acharya <ayan.acharya02@gmail.com>
2024-01-08 23:19:39 +0530

    Removing rangedPrimes() function

--- C and C++/rangedPrime.cpp
+++ C and C++/rangedPrime.cpp
@@ -4,13 +4,8 @@
 #include <stdexcept>
 #include <chrono>
 #include <vector>
-#include <atomic>
-#include <mutex>
 #include <memory>
 #include <cmath>
-
-std::mutex addingLock;
-std::vector<uint32_t> primeBuffer;
 
 bool isPrime(uint32_t num)
 {
@@ -27,50 +22,11 @@ bool isPrime(uint32_t num)
     return true;
 }
 
-void printPrimes(uint32_t threadID, uint32_t lowerLimit, uint32_t upperLimit)
+void printPrimes(uint32_t threadID, std::vector<uint32_t>& list, uint32_t lowerLimit, uint32_t upperLimit)
 {
-    std::cout << "Thread " << threadID << " Created !!" << std::endl;
-
     for(uint32_t i = lowerLimit; i <= upperLimit; i++)
         if(isPrime(i))
-        {
-            addingLock.lock();
-            primeBuffer.push_back(i);
-            addingLock.unlock();
-        }
-}
-
-void rangedPrimes(uint32_t lwrLmt, uint32_t uprLmt, uint32_t numOfThrds)
-{
-    std::cout << "Function Starts!!" << std::endl;
-
-    uint32_t *upprBoundList = new uint32_t[numOfThrds];
-    uint32_t *lwrBoundList = new uint32_t[numOfThrds];
-    uint32_t division = ((uprLmt - lwrLmt) / numOfThrds);
-    // std::vector<std::vector<uint32_t>> primesList (numOfThrds, std::vector<uint32_t>());
-    // std::vector<uint32_t> tempList;
-    std::vector<std::thread> threadList;
-
-    // Dividing the range among all the threads
-    for(uint32_t i = 0; i < numOfThrds; i++)
-        lwrBoundList[i] = lwrLmt + (i * division); // Loop to set the lower bounds for each thread
-
-    for(uint32_t i = 1; i < numOfThrds; i++) // Loop to set the upper bounds for each thread
-        if((lwrBoundList[i] - 1) > uprLmt)
-            upprBoundList[i - 1] = lwrBoundList[i] - 1;
-        else
-            upprBoundList[i - 1] = uprLmt;
-
-    upprBoundList[numOfThrds - 1] = uprLmt;
-
-    // creating threads
-    for(uint32_t i = 0; i < numOfThrds; i++)
-        threadList.push_back(std::thread(printPrimes, i, lwrLmt, uprLmt));
-
-    for(uint32_t i = 0; i < threadList.size(); i++)
-        threadList[i].join();
-
-    std::cout << "Function Ends!!" << std::endl;
+            list.push_back(i);
 }
 
 int main()
@@ -79,15 +35,50 @@ int main()
 
     std::cout << "Upper : " << rngUpper << "\nLower : " << rngLower << std::endl;
 
-    for(uint32_t i = 1; i < threadCount; i++)
-    {
-        auto start = std::chrono::steady_clock::now(); // Starting clock
-        rangedPrimes(rngLower, rngUpper, i);
-        auto stop = std::chrono::steady_clock::now(); // Stopping clock
-        primeBuffer.clear();
-        std::chrono::duration<double> diff = stop - start;
-        std::cout << i << " " << std::chrono::duration<double, std::milli>(diff).count() << std::endl;
-    }
+    std::cout << "Enter the number of threads : ";
+    std::cin >> threadCount;
+
+    uint32_t *upprBoundList = new uint32_t[threadCount];
+    uint32_t *lwrBoundList = new uint32_t[threadCount];
+    uint32_t division = ((rngUpper - rngLower) / threadCount);
+    std::vector<uint32_t> primeList;
+    std::vector<std::vector<uint32_t>> valueList (threadCount);
+    std::vector<std::thread> threadList (threadCount);
+
+    // Dividing the range among all the threads
+    for(uint32_t i = 0; i < threadCount; i++)
+        lwrBoundList[i] = rngLower + (i * division); // Loop to set the lower bounds for each thread
+
+    for(uint32_t i = 1; i < threadCount; i++) // Loop to set the upper bounds for each thread
+        if((lwrBoundList[i] - 1) < rngUpper)
+            upprBoundList[i - 1] = lwrBoundList[i] - 1;
+        else
+            upprBoundList[i - 1] = rngUpper;
+
+    upprBoundList[threadCount - 1] = rngUpper;
+
+    for(uint32_t i = 0; i < threadCount; i++)
+        std::cout << lwrBoundList[i] << "    " << upprBoundList[i] << std::endl;
+
+    auto start = std::chrono::steady_clock::now(); // Starting clock
+
+    // creating threads
+    for(uint32_t i = 0; i < threadCount; i++)
+        threadList[i] = std::thread(printPrimes, i, std::ref(valueList[i]), lwrBoundList[i], upprBoundList[i]);
+
+    for(uint32_t i = 0; i < threadList.size(); i++)
+        threadList[i].join();
+
+    auto stop = std::chrono::steady_clock::now(); // Stopping clock
+
+    // Joining back the seperate lists to form the final primes list
+    for(uint32_t i = 0; i < threadCount; i++)
+        for(uint32_t j = 0; j < valueList[i].size(); j++)
+            primeList.push_back(valueList[i][j]);
+
+    std::chrono::duration<double> diff = stop - start;
+
+    std::cout << "Time : " << std::chrono::duration<double, std::milli>(diff).count() << std::endl;
 
     return 0;
 }
