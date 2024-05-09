b17184eacaab6d24636acd6341b6560a8af9243c
ayan-acharya <ayan.acharya02@gmail.com>
2024-05-09 22:12:32 +0530

    Applying SMPD on matrix multiplication

--- C and C++/multithreading/matrixParallel.cpp
+++ C and C++/multithreading/matrixParallel.cpp
@@ -33,37 +33,98 @@ void parallelMulti(const std::vector<int>& m1,
                 int number_of_threads,
                 std::vector<int>& m3)
 {
+    /*
+    Parallel Matrix Multiplication:
+    number of threads: 1   time taken = 3068.49    performance = 1
+    number of threads: 2   time taken = 382.393    performance = 8.02444
+    number of threads: 3   time taken = 120.953    performance = 25.3693
+    number of threads: 4   time taken = 60.8013    performance = 50.4675
+    number of threads: 5   time taken = 24.5382    performance = 125.05
+    number of threads: 6   time taken = 16.4584    performance = 186.439
+    number of threads: 7   time taken = 10.5425    performance = 291.058
+    number of threads: 8   time taken = 8.15854    performance = 376.108
+    number of threads: 9   time taken = 5.6638    performance = 541.772
+    number of threads: 10   time taken = 4.51626    performance = 679.431
+    number of threads: 11   time taken = 2.87942    performance = 1065.66
+    number of threads: 12   time taken = 2.68077    performance = 1144.63
+    number of threads: 13   time taken = 2.43234    performance = 1261.54
+    number of threads: 14   time taken = 1.86055    performance = 1649.24
+    number of threads: 15   time taken = 2.26283    performance = 1356.04
+    number of threads: 16   time taken = 1.90193    performance = 1613.36
+    */
+    // With SMPD
+    int id = 0;
+
+    omp_set_num_threads(number_of_threads);
+
+    #pragma omp parallel
+    {
+        id = omp_get_thread_num();
+
+        #pragma omp for
+            for(int i = id; i < size; i += number_of_threads) // Row     
+            {
+                for(int j = id; j < size; j += number_of_threads) // Column   
+                {
+                    for(int k = 0; k < size; k++)
+                    {
+                        m3[j * size + i] += m1[i * size + k] * m2[k * size + j];
+                    }
+                }
+            }
+    }
+
+
+    /*
+    Parallel Matrix Multiplication:
+    number of threads: 1   time taken = 3297.32    performance = 1
+    number of threads: 2   time taken = 1752.42    performance = 1.88159
+    number of threads: 3   time taken = 1159.06    performance = 2.84482
+    number of threads: 4   time taken = 978.485    performance = 3.36982
+    number of threads: 5   time taken = 729.491    performance = 4.52003
+    number of threads: 6   time taken = 614.1    performance = 5.36935
+    number of threads: 7   time taken = 561.196    performance = 5.87552
+    number of threads: 8   time taken = 516.232    performance = 6.38728
+    number of threads: 9   time taken = 438.73    performance = 7.5156
+    number of threads: 10   time taken = 420.743    performance = 7.8369
+    number of threads: 11   time taken = 401.967    performance = 8.20296
+    number of threads: 12   time taken = 371.116    performance = 8.88488
+    number of threads: 13   time taken = 324.063    performance = 10.1749
+    number of threads: 14   time taken = 322.201    performance = 10.2337
+    number of threads: 15   time taken = 323.114    performance = 10.2048
+    number of threads: 16   time taken = 293.603    performance = 11.2305
+    
+    // Without SMPD
     omp_set_num_threads(number_of_threads);
 
     #pragma omp parallel
     {
         #pragma omp for
-            for(int i = 0; i < size; i++) // Row
+            for(int i = 0; i < size; i++) // Row     
             {
-                for(int j = 0; j < size; j++) // Column
+                for(int j = 0; j < size; j++) // Column   
                 {
-                    int product = 0;
-
                     for(int k = 0; k < size; k++)
                     {
-                        product += m1[i * size + k] * m2[k * size + j];
+                        m3[size * j + i] += m1[i * size + k] * m2[k * size + j];
                     }
-
-                    m3[size * j + i] = product;
                 }
             }
     }
+    */
 }
 
 
 int main()
 {
-    const int size = 100;
+    const int size = 1024;
+    double serialTime = 0.0;
+    double parallelTime = 0.0;
 
     std::random_device device;
     std::mt19937 rng(device());
     std::uniform_int_distribution<std::mt19937::result_type> 
-                        random50(-100, 100);
+                        random50(-1000, 1000);
 
     // Matrices are stored row wise
     std::vector<int> m1(size * size);
@@ -84,9 +145,9 @@ int main()
 
     auto end = std::chrono::steady_clock::now();
 
-    std::cout << "Linear time taken = " << 
-    std::chrono::duration<double, std::micro>(end - start).count() 
-    << std::endl;
+    serialTime = std::chrono::duration<double, std::milli>(end - start).count();
+
+    std::cout << "Linear time taken = " << serialTime << std::endl;
 
     // Block for parallel multiplication
     std::cout << "\nParallel Matrix Multiplication:\n";
@@ -99,9 +160,12 @@ int main()
 
         end = std::chrono::steady_clock::now();
 
-        std::cout << "number of threads: " << i << "   time taken = " << 
-        std::chrono::duration<double, std::micro>(end - start).count() 
-        << std::endl;
+        if(i == 1)
+            serialTime = std::chrono::duration<double, std::milli>(end - start).count();
+
+        parallelTime = std::chrono::duration<double, std::milli>(end - start).count();
+
+        std::cout << "number of threads: " << i << "   time taken = " << parallelTime << "    performance = " << serialTime / parallelTime << std::endl;
     }
 
     return 0;
